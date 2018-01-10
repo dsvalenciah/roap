d47e1e79068424d0c650042e23eed305457eb44e
dsvalenciah <dsvalenciah@unal.edu.co>
2018-01-09 22:37:49 -0500

    refactor for routes, add object routes

--- back/app.py
+++ back/app.py
@@ -1,9 +1,10 @@
 import json
 
 from resources import user
-from resources import metadata
+from resources import object_
+from resources import object_metadata
 
-from config.learning_object import learning_object_schema_populate
+from config.object_metadata import learning_object_schema_populate
 
 import falcon
 
@@ -11,13 +12,14 @@ learning_object_schema_populate()
 
 api = falcon.API()
 
-api.add_route('/back/learn-obj-schema/{field_id}', metadata.ModifierResource())
-api.add_route('/back/learn-obj-schema-create', metadata.CreatorResource())
-api.add_route('/back/learn-obj-schema-search', metadata.FinderResource())
-api.add_route(
-    '/back/validate-learning-object', metadata.ValidateLearningObject()
-)
+api.add_route('/back/obj-meta', object_metadata.Query())
+api.add_route('/back/obj-meta-create', object_metadata.Create())
+api.add_route('/back/obj-meta/{field_id}', object_metadata.Modify())
 
-api.add_route('/back/user/{user_id}', user.ModifierResource())
-api.add_route('/back/user-create', user.CreatorResource())
-api.add_route('/back/user-search', user.FinderResource())
+api.add_route('/back/obj', object_.Query())
+api.add_route('/back/obj-create/{user_id}', object_.Create())
+api.add_route('/back/obj/{user_id}/{object_id}', object_.Modify())
+
+api.add_route('/back/user', user.Query())
+api.add_route('/back/user-create', user.Create())
+api.add_route('/back/user/{user_id}', user.Modify())
