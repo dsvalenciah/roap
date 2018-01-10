535adb10e7b9b72c26b40e302cec5fdf3bc0f25a
dsvalenciah <dsvalenciah@unal.edu.co>
2018-01-09 22:38:50 -0500

    refactor resources

--- back/resources/user.py
+++ back/resources/user.py
@@ -3,7 +3,7 @@ import os
 from uuid import uuid4
 from datetime import datetime
 
-from schemas.user import User
+from schemas.user import is_valid_user
 from utils.req_to_json import req_to_json
 
 from pymongo import MongoClient
@@ -14,76 +14,87 @@ import falcon
 client = MongoClient(os.getenv('DB_HOST'), 27017)
 db = client.roap
 
-user_schema = User()
 
-
-class CreatorResource:
+class Create:
     def on_post(self, req, resp):
-        user = req_to_json(req)
-        user.update({
-            '_id': str(uuid4().hex),
-            'created': str(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
-        })
-
-        valid_user, errors = user_schema.load(user)
-
-        if errors:
-            resp.body = json.dumps(errors)
-            resp.status = falcon.HTTP_400
-            return
-
-        result = db.users.insert_one(valid_user)
-
-        if not result.acknowledged:
-            resp.body = json.dumps({
-                'message': 'Create user is not posible'
+        if req.headers.get("AUTHORIZATION"):
+            user = req_to_json(req)
+            user.update({
+                '_id': str(uuid4().hex),
+                'created': str(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
             })
-            resp.status = falcon.HTTP_404
-            return
+            valid_user, errors = is_valid_user(user)
+            if errors:
+                resp.body = json.dumps({"errors": errors})
+                resp.status = falcon.HTTP_400
+            else:
+                result = db.users.insert_one(valid_user)
+                if not result.acknowledged:
+                    resp.status = falcon.HTTP_400
+                else:
+                    resp.status = falcon.HTTP_201
+        else:
+            resp.status = falcon.HTTP_401
 
-        resp.status = falcon.HTTP_201
-        resp.body = json.dumps(
-            {'message': 'Ok', 'user': dumps(valid_user)}
-        )
-
-class ModifierResource:
+class Modify:
     def on_put(self, req, resp, user_id):
-        user = req_to_json(req)
-
-        result = db.users.update_one(
-            {'_id': user_id},
-            {'$set': user}
-        )
-
-        if not result.modified_count:
-            resp.body = json.dumps({
-                'message': 'The specified user id is not found on database'
-            })
-            resp.status = falcon.HTTP_404
-            return
-
-        resp.status = falcon.HTTP_200
-        resp.body = json.dumps({'message': 'Ok'})
+        if req.headers.get("AUTHORIZATION"):
+            user = req_to_json(req)
+            result = db.users.update_one(
+                {'_id': user_id},
+                {'$set': user}
+            )
+            if not result.modified_count:
+                resp.body = json.dumps({
+                    'message': 'The specified user id is not found on database'
+                })
+                resp.status = falcon.HTTP_404
+            else:
+                resp.status = falcon.HTTP_200
+                resp.body = json.dumps({'message': 'Ok'})
+        else:
+            resp.status = falcon.HTTP_401
 
     def on_delete(self, req, resp, user_id):
-        result = db.users.delete_one({'_id': user_id})
-        if not result.deleted_count:
-            resp.body = json.dumps({
-                'message': 'The specified user id is not found on database'
-            })
-            resp.status = falcon.HTTP_404
-            return
+        if req.headers.get("AUTHORIZATION"):
+            result = db.users.delete_one({'_id': user_id})
+            if not result.deleted_count:
+                resp.body = json.dumps({
+                    'message': 'The specified user id is not found on database'
+                })
+                resp.status = falcon.HTTP_404
+            else:
+                resp.status = falcon.HTTP_200
+                resp.body = json.dumps({'message': 'Ok'})
+        else:
+            resp.status = falcon.HTTP_401
 
-        resp.status = falcon.HTTP_200
-        resp.body = json.dumps({'message': 'Ok'})
-
-class FinderResource:
-    def on_post(self, req, resp):
-        query = req_to_json(req)
-        if not query:
-            resp.body = json.dumps(json.loads(dumps(db.users.find())))
-            resp.status = falcon.HTTP_200
-            return
-
-        resp.status = falcon.HTTP_200
-        resp.body = json.dumps(query)
+class Query:
+    def on_get(self, req, resp):
+        if req.headers.get("AUTHORIZATION"):
+            query_params = req.params
+            if not query_params:
+                resp.body = json.dumps(json.loads(dumps(db.users.find())))
+                resp.status = falcon.HTTP_200
+            else:
+                enabled_fields = [
+                    "title", "description", "keyword", "name"
+                ]
+                correct_fields = map(
+                    is_correct_parameter, query_params.values()
+                )
+                if not False in correct_fields:
+                    fields_to_use = [
+                        {x: {"$regex": f".*{query_params.get(x)}.*"}}
+                        for x in query_params.keys()
+                        if x in enabled_fields
+                    ]
+                    query = {"$and": fields_to_use}
+                    resp.body = json.dumps(json.loads(dumps(
+                        db.users.find(query)
+                    )))
+                    resp.status = falcon.HTTP_200
+                else:
+                    resp.status = falcon.HTTP_400
+        else:
+            resp.status = falcon.HTTP_401
