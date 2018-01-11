3df962efa050489f80d7443248ab4714e6aa20c1
dsvalenciah <dsvalenciah@unal.edu.co>
2018-01-11 01:00:34 -0500

    create roap class that add more tools for roap app

--- back/app.py
+++ back/app.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 from resources import user
 from resources import object_
@@ -8,18 +9,41 @@ from config.object_metadata import learning_object_schema_populate
 
 import falcon
 
+from pymongo import MongoClient
+
 learning_object_schema_populate()
 
-api = falcon.API()
+class Roap():
+    def __init__(self, db_host='DB_HOST', db_port=27017, db_name="roap"):
+        self.client = MongoClient(os.getenv(db_host), db_port)
+        self.db = self.client[db_name]
 
-api.add_route('/back/obj-meta', object_metadata.Query())
-api.add_route('/back/obj-meta-create', object_metadata.Create())
-api.add_route('/back/obj-meta/{field_id}', object_metadata.Modify())
+        object_metadata.set_db_client(self.db)
+        object_.set_db_client(self.db)
+        user.set_db_client(self.db)
 
-api.add_route('/back/obj', object_.Query())
-api.add_route('/back/obj-create/{user_id}', object_.Create())
-api.add_route('/back/obj/{user_id}/{object_id}', object_.Modify())
+        self.api = falcon.API()
 
-api.add_route('/back/user', user.Query())
-api.add_route('/back/user-create', user.Create())
-api.add_route('/back/user/{user_id}', user.Modify())
+        self.api.add_route('/back/obj-meta', object_metadata.Query())
+        self.api.add_route('/back/obj-meta-create', object_metadata.Create())
+        self.api.add_route(
+            '/back/obj-meta/{field_id}', object_metadata.Modify()
+        )
+
+        self.api.add_route('/back/obj', object_.Query())
+        self.api.add_route('/back/obj-create/{user_id}', object_.Create())
+        self.api.add_route('/back/obj/{user_id}/{object_id}', object_.Modify())
+
+        self.api.add_route('/back/user', user.Query())
+        self.api.add_route('/back/user-create', user.Create())
+        self.api.add_route('/back/user/{user_id}', user.Modify())
+
+    def get_db(self):
+        return self.db
+
+    def get_api(self):
+        return self.api
+
+
+roap = Roap()
+api = roap.get_api()
