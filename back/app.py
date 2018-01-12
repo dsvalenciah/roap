1bceae6a2f11bf8a820d7fb18a9b53609525e623
dsvalenciah <dsvalenciah@unal.edu.co>
2018-01-12 17:19:10 -0500

    change learning object resources body

--- back/app.py
+++ back/app.py
@@ -2,7 +2,7 @@ import json
 import os
 
 from resources import user
-from resources import object_
+from resources import learning_object
 from resources import object_metadata
 
 from config.object_metadata import learning_object_schema_populate
@@ -19,7 +19,7 @@ class Roap():
         self.db = self.client[db_name]
 
         object_metadata.set_db_client(self.db)
-        object_.set_db_client(self.db)
+        learning_object.set_db_client(self.db)
         user.set_db_client(self.db)
 
         self.api = falcon.API()
@@ -27,18 +27,18 @@ class Roap():
         self.api.add_route('/back/user', user.UserCollection())
         self.api.add_route('/back/user/{uid}', user.User())
 
-        self.api.add_route('/back/object', object_.LearningObjectCollection())
-        self.api.add_route('/back/object/{uid}', object_.LearningObject())
+        self.api.add_route(
+            '/back/object', learning_object.LearningObjectCollection()
+        )
+        self.api.add_route(
+            '/back/object/{uid}', learning_object.LearningObject()
+        )
 
         self.api.add_route('/back/obj-meta', object_metadata.Query())
         self.api.add_route('/back/obj-meta-create', object_metadata.Create())
         self.api.add_route(
             '/back/obj-meta/{field_id}', object_metadata.Modify()
         )
-
-        self.api.add_route('/back/obj', object_.Query())
-        self.api.add_route('/back/obj-create/{user_id}', object_.Create())
-        self.api.add_route('/back/obj/{user_id}/{object_id}', object_.Modify())
 
     def get_db(self):
         return self.db
--- back/resources/learning_object.py
+++ back/resources/learning_object.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from uuid import uuid4
 import json
 
-from schemas.object_metadata import is_valid_object
+from schemas.object_metadata import is_valid_learning_object
 from utils.req_to_json import req_to_json
 
 from bson.json_util import dumps
@@ -56,31 +56,18 @@ class LearningObjectCollection(object):
         Create learning object.
         """
         # Notice that, the user id will come in the payload
-
-
-class Create:
-    def on_post(self, req, resp, user_id):
         if req.headers.get("AUTHORIZATION"):
-            object_ = req_to_json(req)
-            valid_object, errors = is_valid_object(object_)
+            learning_object = req_to_json(req)
+            errors = is_valid_object(learning_object)
             if errors:
                 resp.body = json.dumps({"errors": errors})
                 resp.status = falcon.HTTP_400
             else:
-                valid_object.update({'_id': str(uuid4().hex)})
-                result = db.users.update_one(
-                    {"_id": user_id},
-                    {"$push": {"objects": valid_object}}
-                )
-                if not result.matched_count and not result.modified_count:
-                    resp.status = falcon.HTTP_400
-                else:
+                learning_object.update({'_id': uuid4().hex})
+                try:
+                    result = db.learning_objects.insert_one(learning_object)
                     resp.status = falcon.HTTP_201
+                except pymongo.errors.DuplicateKeyError:
+                    resp.status = falcon.HTTP_400
         else:
             resp.status = falcon.HTTP_401
-
-class Modify:
-    pass
-
-class Query:
-    pass
