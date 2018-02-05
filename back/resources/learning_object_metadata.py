4e9128b8bee0a9622b9e122541fa6cbe0efc5232
dsvalenciah <dsvalenciah@unal.edu.co>
2018-02-05 01:35:12 -0500

    Remove unnecessary http methods from Resource

--- back/resources/learning_object_metadata.py
+++ back/resources/learning_object_metadata.py
@@ -4,14 +4,11 @@ Contains necessary Resources to works with learning-object metadata
 fields CRUD operations.
 """
 
-from datetime import datetime
-from uuid import uuid4
 import json
-import re
 
-from schemas.learning_object_metadata import is_valid_schema_field
+from marshmallowjson.marshmallowjson import Definition
+from marshmallowjson.exceptions import ValidationError
 from utils.req_to_dict import req_to_dict
-from utils.request_param import is_correct_parameter
 
 from bson.json_util import dumps
 
@@ -28,14 +25,12 @@ def set_db_client(db_client):
 
 
 class LearningObjectMetadata(object):
-    """Deal with single learning-object-metadata-field."""
+    """Deal with the whole collection of learning-object-metadata-fields."""
 
-    def on_get(self, req, resp, uid):
+    def on_get(self, req, resp):
         """Get a single learning-object-metadata-field."""
         if req.headers.get('AUTHORIZATION'):
-            result = db.learning_object_metadata.find_one(
-                {'_id': uid}
-            )
+            result = db.learning_object_metadata.find_one({'_id': 'lom'})
             if not result:
                 resp.status = falcon.HTTP_404
             else:
@@ -44,89 +39,22 @@ class LearningObjectMetadata(object):
         else:
             resp.status = falcon.HTTP_401
 
-    def on_put(self, req, resp, uid):
+    def on_put(self, req, resp):
         """Update learning-object-metadata-field."""
         if req.headers.get('AUTHORIZATION'):
-            field = req_to_dict(req)
-            # TODO: validate new learning-object-metadata-field
-            result = db.learning_object_metadata.update_one(
-                {'_id': uid},
-                {'$set': field}
-            )
-
-            if not result.modified_count:
-                resp.status = falcon.HTTP_404
-            else:
-                resp.status = falcon.HTTP_200
-        else:
-            resp.status = falcon.HTTP_401
-
-    def on_delete(self, req, resp, uid):
-        """Delete single learning-object-metadata-field."""
-        if req.headers.get('AUTHORIZATION'):
-            result = db.learning_object_metadata.delete_one(
-                {'_id': uid}
-            )
-            if not result.deleted_count:
-                resp.status = falcon.HTTP_404
-            else:
-                resp.status = falcon.HTTP_200
-        else:
-            resp.status = falcon.HTTP_401
-
-
-class LearningObjectMetadataCollection(object):
-    """Deal with the whole collection of learning-object-metadata-fields."""
-
-    def on_get(self, req, resp):
-        """Get all users (maybe filtered, and paginated)."""
-        if req.headers.get('AUTHORIZATION'):
-            query_params = req.params
-            if not query_params:
-                resp.body = dumps(
-                    db.learning_object_metadata.find()
+            lom = req_to_dict(req).get('lom')
+            try:
+                Definition(lom)
+                result = db.learning_object_metadata.update_one(
+                    {'_id': 'lom'},
+                    {'$set': {'lom': lom}}
                 )
-                resp.status = falcon.HTTP_200
-            else:
-                # TODO: add offset, count as a required params
-                enabled_fields = [
-                    'title', 'description', 'keyword', 'name'
-                    # TODO: add 'start', 'end' date range
-                    # add 'offset', 'count'
-                ]
-                correct_fields = map(
-                    is_correct_parameter, query_params.values()
-                )
-                if False not in correct_fields:
-                    fields_to_use = [
-                        {x: {'$regex': f'.*{query_params.get(x)}.*'}}
-                        for x in query_params.keys()
-                        if x in enabled_fields
-                    ]
-                    query = {'$and': fields_to_use}
-                    resp.body = dumps(
-                        db.learning_object_metadata.find(query)
-                    )
-                    resp.status = falcon.HTTP_200
-                else:
-                    resp.status = falcon.HTTP_400
-        else:
-            resp.status = falcon.HTTP_401
-
-    def on_post(self, req, resp):
-        """Create learning-object-metadata-field."""
-        if req.headers.get('AUTHORIZATION'):
-            field = req_to_dict(req)
-            field.update({'_id': str(uuid4().hex)})
-            errors = is_valid_schema_field(field)
-            if errors:
-                resp.body = json.dumps({'errors': errors})
-                resp.status = falcon.HTTP_400
-            else:
-                result = db.learning_object_metadata.insert_one(field)
                 if not result.acknowledged:
                     resp.status = falcon.HTTP_400
                 else:
                     resp.status = falcon.HTTP_201
+            except ValidationError as e:
+                resp.body = json.dumps({'errors': str(e)})
+                resp.status = falcon.HTTP_400
         else:
             resp.status = falcon.HTTP_401
