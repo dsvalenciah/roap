a7b20c1682a1679a46dfcd5481d3188469721929
dsvalenciah <dsvalenciah@unal.edu.co>
2018-02-11 00:11:23 -0500

    Refactor user schema

--- back/schemas/user.py
+++ back/schemas/user.py
@@ -9,21 +9,27 @@ from marshmallow import Schema, fields, validate
 class User(Schema):
     """Definition for user schema."""
 
-    _id = fields.Str(required=True)
+    # TODO: add keywords and notifications?
+
+    _id = fields.UUID(required=True)
     name = fields.Str(required=True)
     email = fields.Str(
         required=True,
         validate=validate.Email(error='Not a valid email address')
     )
     role = fields.Str(required=True, validate=validate.OneOf(
-        ['administrator', 'expert', 'creator', 'external']
+        ['administrator', 'expert', 'creator', 'external', 'unknown']
+    ))
+    status = fields.Str(required=True, validate=validate.OneOf(
+        ['active', 'inactive']
     ))
     created = fields.DateTime(required=True, format='%Y-%m-%d %H:%M:%S')
-    modified = fields.Str(required=False)
+    modified = fields.DateTime(required=True, format='%Y-%m-%d %H:%M:%S')
+    last_activity = fields.DateTime(required=True, format='%Y-%m-%d %H:%M:%S')
 
 user_schema = User()
 
 
 def is_valid_user(user):
     """Check if user and his schema is matching."""
-    return user_schema.load(user)
+    return user_schema.validate(user)
