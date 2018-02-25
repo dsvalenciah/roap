2db4e7ada9fdfae19e42473602d423f38bc283db
dsvalenciah <dsvalenciah@unal.edu.co>
2018-02-25 00:25:09 -0500

    Fix json file path

--- back/config/collections_category.py
+++ back/config/collections_category.py
@@ -6,18 +6,11 @@ collections categories.
 
 import collections
 import json
-import os
 
 from bson.json_util import dumps
 
-from pymongo import MongoClient
 
-
-client = MongoClient(os.getenv('DB_HOST'), 27017)
-db = client.roap
-
-
-def collections_category_populate():
+def collections_category_populate(db):
     """Populate database with default collections categories."""
     collections_category_schema = json.loads(dumps(
         db.collections_category.find({'_id': 'collections_category'})
@@ -25,7 +18,7 @@ def collections_category_populate():
 
     if not collections_category_schema:
         collections_category_schema = json.load(
-            open('data/collections_category.json'),
+            open('config/data/collections_category.json'),
             object_pairs_hook=collections.OrderedDict
         )
         db.collections_category.insert_one(
