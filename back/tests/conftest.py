de9ec4aafd19a4d87c3506966ace686c8f62af90
dsvalenciah <dsvalenciah@unal.edu.co>
2018-03-04 17:55:28 -0500

    Remove unnecesary fixtures

--- back/tests/conftest.py
+++ back/tests/conftest.py
@@ -45,34 +45,8 @@ def one_xml_lom():
 
 
 @pytest.fixture(scope='module')
-def many_xml_lom():
-    """Create a file xml file handler with many learning objects."""
-    return open(get_file_path('data/many_learning_objects.xml')).read()
-
-
-@pytest.fixture(scope='module')
-def one_json_lom():
-    """Create a file json file handler with one learning object."""
-    return open(get_file_path('data/one_learning_object.json'))
-
-
-@pytest.fixture(scope='module')
-def many_json_lom():
-    """Create a file json file handler with many learning objects."""
-    return open(get_file_path('data/many_learning_objects.json'))
-
-
-@pytest.fixture(scope='module')
 def one_dict_lom():
     """Create a dict with one learning object."""
     return json.load(
         open(get_file_path('data/one_learning_object.json'))
-    )
-
-
-@pytest.fixture(scope='module')
-def many_dict_lom():
-    """Create a list of dicts with learning objects."""
-    return json.load(
-        open(get_file_path('data/may_learning_objects.json'))
     )
