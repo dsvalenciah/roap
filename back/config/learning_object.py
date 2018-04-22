d4d4bc0ffab214209cad4cdbd197a82843cc60b0
dsvalenciah <dsvalenciah@unal.edu.co>
2018-04-21 21:33:48 -0500

    Add learning objects file manager

--- back/config/learning_object.py
+++ back/config/learning_object.py
@@ -10,6 +10,7 @@ from exceptions.learning_object import (
     LearningObjectSchemaError, LearningObjectMetadataSchemaError
 )
 
+from utils.storage import StorageUnit
 from utils.learning_object import LearningObject
 from utils.learning_object import LearningObjectScore
 from utils.xml_to_dict import xml_to_dict
@@ -18,6 +19,7 @@ from utils.xml_to_dict import xml_to_dict
 def learning_object_populate(db):
     """Populate database with default learning objects."""
     # TODO: fix category
+    storage = StorageUnit()
     learning_object_manager = LearningObject(db)
     learning_object_score_manager = LearningObjectScore(db)
     list_files_path = glob.glob("config/data/learning_objects_xml/*.xml")
@@ -38,6 +40,9 @@ def learning_object_populate(db):
     for file_path in list_files_path:
         _id, _ = file_path.split('/')[-1].split('.')
         learning_object = db.learning_objects.find_one({'_id': _id})
+        learing_object_file_path = glob.glob(
+            f"config/data/files/{_id}.*"
+        )[0]
         if not learning_object:
             learning_object_metadata = None
             try:
@@ -46,14 +51,14 @@ def learning_object_populate(db):
                         file.read()
                     )
             except Exception as e:
-                print(e)
+                raise ValueError(e)
             if learning_object_metadata:
                 try:
                     role = [
                         'administrator', 'expert', 'creator'
                     ][randint(0, 2)]
                     learning_object_manager.insert_one(
-                        {
+                        learning_object={
                             'lom': learning_object_metadata,
                             'category': [
                                 "Educacion", "Medicina", "Fisica"
@@ -63,6 +68,14 @@ def learning_object_populate(db):
                             'deleted': False,
                             'role': role,
                             '_id': 'ee6a11aee52b4e64b4a6a14d42ff49da'
+                        },
+                        file={
+                            'file_extension': (
+                                learing_object_file_path.split('.')[-1]
+                            ),
+                            'file_content': open(
+                                learing_object_file_path, "rb"
+                            ).read()
                         },
                         ignore_schema=True,
                         _id=_id
@@ -77,4 +90,4 @@ def learning_object_populate(db):
                         randint(1, 5)
                     )
                 except LearningObjectMetadataSchemaError as e:
-                    print(e)
+                    raise ValueError(e.args[0])
